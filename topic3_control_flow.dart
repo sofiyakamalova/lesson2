b01898e9b5ff42fe01abfc7ab8995a3573b44cfe
sofiyakamalova <sofiyakamalova1805@gmail.com>
2023-11-04 14:20:34 +0600

    first commit

---
+++ topic3_control_flow.dart
@@ -0,0 +1,143 @@
+import 'dart:io';
+
+void main() {
+/*
+TASK1
+  int n = int.parse(stdin.readLineSync()!);
+  int a = int.parse(stdin.readLineSync()!);
+//a
+  if ((n % 10 == 4 || n % 10 == 7) || (n ~/ 10 == 4 || n ~/ 10 == 7)) {
+    print('Yes');
+  } else {
+    print('No');
+  }
+
+//b
+  if ((n % 10 == 3 || n % 10 == 6 || n % 10 == 9) ||
+      (n ~/ 10 == 3 || n ~/ 10 == 6 || n ~/ 10 == 9)) {
+    print('Yes');
+  } else {
+    print('No');
+  }
+  
+//c
+  if (n % 10 == a || n ~/ 10 == a) {
+    print('Yes');
+  } else {
+    print('No');
+  }
+
+  
+ 
+  TASK2 A
+  int n = int.parse(stdin.readLineSync()!);
+
+  while (n != 0) {
+    int l = n % 10;
+    if (l == 4 || l == 7) {
+      print('Yes');
+      break;
+    }
+    n = n ~/ 10;
+  }
+  if (n == 0) {
+    print('No');
+  }
+  
+//B
+  int n = int.parse(stdin.readLineSync()!);
+
+  while (n != 0) {
+    int l = n % 10;
+    if (l == 3 || l == 6 || l == 9) {
+      print('Yes');
+      break;
+    }
+    n = n ~/ 10;
+  }
+  if (n == 0) {
+    print('No');
+  }
+  
+//C
+  int n = int.parse(stdin.readLineSync()!);
+  print("Напишите число n");
+  int b = int.parse(stdin.readLineSync()!);
+
+  while (n != 0) {
+    int l = n % 10;
+    if (l == b) {
+      print('Yes');
+      break;
+    }
+    n = n ~/ 10;
+  }
+  if (n == 0) {
+    print('No');
+  }
+
+
+  TASK3
+  int n = int.parse(stdin.readLineSync()!);
+  int a = n ~/ 1000;
+  int b = n ~/ 100 % 10;
+  int c = n ~/ 10 % 10;
+  int d = n % 10;
+
+  if (a == d && b == c) {
+    print('Yes');
+  } else {
+    print('No');
+  }
+  
+TASK4
+  int n = int.parse(stdin.readLineSync()!);
+  int a = n ~/ 1000;
+  int b = n ~/ 100 % 10;
+  int c = n ~/ 10 % 10;
+  int d = n % 10;
+
+  if (a == b && a == c) {
+    print('Yes');
+  } else if (a == b && a == d) {
+    print('Yes');
+  } else if (a == c && a == d) {
+    print('Yes');
+  } else if (b == c && b == d) {
+    print("Yes");
+  } else {
+    print("No");
+  }
+  TASK5
+  int sum = 0;
+  for (int i = 100; i <= 500; ++i) {
+    sum = sum + i;
+  }
+  print(sum);
+  
+TASK7 A
+  int sum = 0;
+  int count = 0;
+  for (int i = 1; i <= 1000; ++i) {
+    sum = sum + i;
+    count += 1;
+  }
+  int ave = sum ~/ count;
+  print(ave);
+
+
+//B
+
+  int a = int.parse(stdin.readLineSync()!);
+  int b = int.parse(stdin.readLineSync()!);
+
+  int sum = 0;
+  int count = 0;
+  for (int i = a; i <= b; ++i) {
+    sum = sum + i;
+    count += 1;
+  }
+  int ave = sum ~/ count;
+  print(ave);
+  */
+}
